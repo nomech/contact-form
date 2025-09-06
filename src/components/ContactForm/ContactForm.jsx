f0d0158839abf0a25277d9f8fdf274221a3aa8a8
Christoffer <mech1803@gmail.com>
2025-09-07 00:37:44 +0200

    Add form validation using Zod and integrate with React Hook Form
    - Updated package.json and package-lock.json to include @hookform/resolvers and zod.
    - Implemented form validation schema in ContactForm component.
    - Enhanced InputField component to display validation errors.
    - Styled error messages in InputField.module.css.

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,28 +1,69 @@
 import styles from './ContactForm.module.css';
 import Button from '../Button/Button';
 import InputField from '../InputField/InputField';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const formSchema = z.object({
+	firstName: z.string().min(1, 'This field is required'),
+	lastName: z.string().min(1, 'This field is required'),
+	email: z.email('Please enter a valid email address'),
+	message: z.string().min(10, 'This field is required'),
+	consent: z.boolean().refine((val) => val === true, {
+		message: 'You must consent to proceed',
+	}),
+});
+
+const defaultvalue = {
+	firstName: '',
+	lastName: '',
+	email: '',
+	general: false,
+	support: false,
+	message: '',
+	consent: false,
+};
 
 const ContactForm = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: zodResolver(formSchema),
+		defaultvalue: defaultvalue,
+	});
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
 	return (
-		<form className={styles.form}>
+		<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
 			<h2 className={styles.formTitle}>Contact us</h2>
 			<fieldset className={styles.fieldset}>
 				<div className={styles.groupedInput}>
 					<InputField
 						label={'First Name *'}
-						id={'FirstName'}
-						register={'firstName'}
-						mandatory={true}
+						id={'firstName'}
+						register={register}
+						errors={errors}
 					/>
 					<InputField
 						label={'Last Name *'}
-						id={'LastName'}
-						register={'firstName'}
-						mandatory={true}
+						id={'lastName'}
+						register={register}
+						errors={errors}
 					/>
 				</div>
 				<div className={styles.groupedInput}>
-					<InputField label={'Email Address *'} id={'email'} mandatory={true} />
+					<InputField
+						label={'Email Address *'}
+						id={'email'}
+						register={register}
+						errors={errors}
+					/>
 				</div>
 			</fieldset>
 			<fieldset className={styles.fieldset}>
@@ -33,12 +74,16 @@ const ContactForm = () => {
 						type={'radio'}
 						name={'query'}
 						id={'general'}
+						register={register}
+						errors={errors}
 					/>
 					<InputField
 						label={'Support Request'}
 						type={'radio'}
 						name={'query'}
 						id={'support'}
+						register={register}
+						errors={errors}
 					/>
 				</div>
 			</fieldset>
@@ -47,15 +92,19 @@ const ContactForm = () => {
 					<InputField
 						label={'Message *'}
 						type={'textarea'}
-						name={'query'}
-						id={'support'}
+						name={'message'}
+						id={'message'}
+						register={register}
+						errors={errors}
 					/>
 				</div>
 
 				<InputField
 					label={'I consent to being contacted by the team'}
 					type={'checkbox'}
-					id={'support'}
+					id={'consent'}
+					register={register}
+					errors={errors}
 				/>
 			</fieldset>
 			<Button type={'submit'}> Submit </Button>
--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,24 +1,16 @@
-import React from 'react';
 import styles from './InputField.module.css';
-import { ErrorMessage } from '@hookform/error-message';
 
-const InputField = ({
-	type = 'text',
-	label,
-	id,
-	name,
-	register,
-	required = false,
-	max = 10,
-	min = 1,
-	errors,
-}) => {
+const InputField = ({ type = 'text', label, id, name, register, errors }) => {
+	errors && console.log(errors);
+
 	return (
 		<div className={styles.fieldGroup}>
 			<label htmlFor={id}>{label}</label>
-			{type === 'textarea' && <textarea id={id} name={name || id}  />}
-			{type !== 'textarea' && <input type={type} id={id} name={name || id} />}
-			{errors && <ErrorMessage errors={errors} />}
+			{type === 'textarea' && <textarea id={id} name={name || id} {...register(id)} />}
+			{type !== 'textarea' && (
+				<input type={type} id={id} name={name || id} {...register(id)} />
+			)}
+			{errors[id] && <p className={styles.error}>{errors[id].message}</p>}
 		</div>
 	);
 };
